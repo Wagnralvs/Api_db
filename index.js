4568512958c8bac816b7d70ed853a840612ff2fd
wagner alves viana <wagneralvesviana99@gmail.com>
2025-07-24 18:53:55 -0300

    alterção index

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
+
+module.exports = server;
